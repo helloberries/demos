fb2ec43fae2f53a71e43d84c09310377d3d23753
Qing Zhang <peachovo@gmail.com>
2016-04-09 16:11:49 +0800

    Navbar bug fix ;A;

--- shop/js/index.js
+++ shop/js/index.js
@@ -1,6 +1,6 @@
 $(function(){
     // 显示导航下的菜单
-    $('.nav-link:nth-child(2)').mouseover(function () {
+    $('.nav:nth-child(2)').mouseover(function () {
         $('.nav-menu').show();
     }).mouseout(function () {
         $('.nav-menu').hide();
