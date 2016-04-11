fc42d164c77a034a960992b52cd4fbe5a21b18be
Qing Zhang <peachovo@gmail.com>
2016-04-11 08:50:36 +0800

    Feature: Click the small pic to show the big pic

--- shop/js/index.js
+++ shop/js/index.js
@@ -5,4 +5,14 @@ $(function(){
     }).mouseout(function () {
         $('.nav-menu').hide();
     });
+    // 点击小图切换商品图
+    $('.product-preview-img-s').click(function () {
+      clsName = $(this).attr('id');
+      $('.product-preview-img').attr('class', 'product-preview-img');
+      $('.product-preview-img').toggleClass(clsName);
+    });
+    // 选择颜色
+    $('.color-radio-div input').click(function () {
+      console.log($(this).attr('value'));
+    });
 });
